7a4ac4fb147f0c16e02283dfa9ba2fe900805284
Ameya Deshmukh <77532501+Ameya-Deshmukh26@users.noreply.github.com>
2025-08-03 22:59:20 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -42,10 +42,8 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
+            <h1><span className="purple">FIND ME ON</span></h1>
+            
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
